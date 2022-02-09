e548e25e526646b2801641c6580a2839a4998411
andreacrm <andreacramirezm@gmail.com>
2022-02-09 05:02:38 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -2,6 +2,8 @@ import './bootstrap.min.css';
 import './App.css';
 import EmotionTable from './EmotionTable.js';
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 
 class App extends React.Component {
   /*
@@ -10,6 +12,10 @@ class App extends React.Component {
   value of the state, will be returned. The initial input mode
   is set to text
   */
+
+    componentDidMount(){
+        document.title="Sentiment Analyzer"
+    }
   state = {innercomp:<textarea rows="4" cols="50" id="textinput"/>,
             mode: "text",
           sentimentOutput:[],
@@ -49,8 +55,9 @@ class App extends React.Component {
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "black";break;
-          case "negative": color = "black";break;
+          case "positive": color = "green";break;
+          case "neutral": color = "yellow";break;
+          case "negative": color = "red";break;
           default: color = "black";
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
